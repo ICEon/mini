5480b18b7f57498709fbd585274684f6075695ea
ICEon <matrix_eon@yahoo.com>
2017-06-27 14:33:10 -0500

    cinco

--- js/acciones.js
+++ js/acciones.js
@@ -1,12 +1,8 @@
 // JavaScript Document
 var db = null;
-var $todas_categorias = ["ikán","scuela","ve'e","ichi","táꞌán","kiti","so'va ká'ví yó","kuni"];
-
+var $todas_categorias = ["<header>","<body>","<form>"];
 
 $(document).ready(function(e) {
-
-
-
 
   $red=255;
 	$green=35;
@@ -17,63 +13,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 });
 
 function buscar_palabras ($cat) {
-	$consulta = "";
-	$titulo_categoria = "";
-	if ($cat == "fav")
-	 {
-	    $consulta = 'SELECT * FROM palabras WHERE Favorita=1';
-		$titulo_categoria = "Kutoi";
-	 }
-	else
-	 {
-	  $cat = $cat.substring(3);
-	  if ($cat == 0)
-	   {
-	    $consulta = 'SELECT * FROM palabras';
-	   }
-	  else
-	   {
-	    $consulta = 'SELECT * FROM palabras WHERE Categoria=' + $cat;
-	   }
-	 }
-
-	db.transaction(function(tx) {
-    tx.executeSql($consulta, [], function(tx, rs) {
-
-		//recorrer el arreglo y llenar las palabras
-//	  $('#palabras_categoria .ui-controlgroup-controls').html()= "";
-      $('#palabras_categoria').empty();
-	   for (var $i=0; $i<rs.rows.length; $i++)
-	{
-
-$('#palabras_categoria').append('<li data-filtertext="' + rs.rows.item($i).Mixteco + '"><a href="#"><div calss="mostrar"><h4>' + rs.rows.item($i).Mixteco + '</h4><h4>' + rs.rows.item($i).Mexicano  + '</h4></div></a><a href="#" class="btn-favorita" id="p' + rs.rows.item($i).idPalabra + '"></a></li>');
-
-
-
-   
-	}
-	
-	$('#palabras_categoria').listview("refresh");
-	 $('.col-ref').collapsible("refresh");
-      
-    }, function(tx, error) {
-      alert ('SELECT error: ' + error.message);
-    });
-  });
-
- if ($titulo_categoria == "")
-  {
-db.transaction(function(tx) {
-    tx.executeSql("SELECT NombreCategoriaM FROM categorias WHERE CveCategoria=" + $cat , [], function(tx, rs) {
-		$titulo_categoria=rs.rows.item(0).NombreCategoriaM; 
-      
-    }, function(tx, error) {
-      alert ('SELECT error: ' + error.message);
-    });
-  });
-
-  }
-  $('#encabezado_busqueda').text($titulo_categoria);
 
 }
 
@@ -97,7 +36,7 @@ function onDeviceReady() {
   $(document).on( 'tap', '.btn-opciones', function(){
 	  //buscar por las palabras de la opicon 
 //      alert ("opcion");
-   realiza_busqueda ($(this).attr('id'));   
+//   realiza_busqueda ($(this).attr('id'));   
      
    });
    
@@ -134,7 +73,7 @@ function onDeviceReady() {
 
         //location = 0, will copy the db to default SQLite Database Directory
 		
-        window.plugins.sqlDB.copy("appmixteco.db", 0, copysuccess,copyerror);
+        window.plugins.sqlDB.copy("mini.db", 0, copysuccess,copyerror);
 
        
 }
@@ -147,7 +86,7 @@ function copysuccess()
         //open db and run your queries
 //          db = window.sqlitePlugin.openDatabase({name: 'appmixteco.db', location: 'default'});
 
-         db = window.sqlitePlugin.openDatabase({name: 'appmixteco.db', location: 'default', androidDatabaseImplementation: 2});
+         db = window.sqlitePlugin.openDatabase({name: 'mini.db', location: 'default', androidDatabaseImplementation: 2});
 		 obtener_categorias();
 }
 
@@ -157,7 +96,7 @@ function copyerror(e)
 
 		if (e.code ==  516)
 		 {
-           db = window.sqlitePlugin.openDatabase({name: 'appmixteco.db', location: 'default', androidDatabaseImplementation: 2});
+           db = window.sqlitePlugin.openDatabase({name: 'mini.db', location: 'default', androidDatabaseImplementation: 2});
 			 obtener_categorias();
 		 }
         //e.code = 516 => if db exists
@@ -176,7 +115,7 @@ function agregar_categorias ($categorias)
 	   $color = '#'+ $red.toString(16) + $green.toString(16) + $blue.toString(16);
 		 if ($i==0)
 		  {
-		$cadena = "<a href='#busqueda' id='cat0' class='btn-opciones ui-btn ui-corner-all ui-icon-alert ui-btn-icon-right  ui-first-child' style='background-color:" + $color+ "'>Ndi'i</a>";	  
+		$cadena = "<a href='#busqueda' id='cat0' class='btn-opciones ui-btn ui-corner-all ui-icon-alert ui-btn-icon-right  ui-first-child' style='background-color:" + $color+ "'>HTML</a>";	  
 		  }
 		
 		 if ($i> 0 && $i < $categorias)
